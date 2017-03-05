69c4f9835ddc854bcc23b83e01afe6225b3132ae
Giuseppe <giuse.scara@gmail.com>
2017-03-05 17:41:34 +0100

    Mongoose Population

--- rest-server-passport/routes/users.js
+++ rest-server-passport/routes/users.js
@@ -2,30 +2,45 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
-var Verify    = require('./verify');
+var Verify = require('./verify');
 
 /* GET users listing. */
-router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin,function(req, res, next) {
+router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
   User.find({}, function (err, user) {
-        if (err) throw err;
-        res.json(user);
-    });
+    if (err) throw err;
+    res.json(user);
+  });
 });
 
-router.post('/register', function(req, res) {
-    User.register(new User({ username : req.body.username }),
-      req.body.password, function(err, user) {
-        if (err) {
-            return res.status(500).json({err: err});
-        }
-        passport.authenticate('local')(req, res, function () {
-            return res.status(200).json({status: 'Registration Successful!'});
+router.post('/register', function (req, res) {
+  User.register(new User({
+      username: req.body.username
+    }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        return res.status(500).json({
+          err: err
         });
+      }
+      if (req.body.firstname) {
+        user.firstname = req.body.firstname;
+      }
+      if (req.body.lastname) {
+        user.lastname = req.body.lastname;
+      }
+      user.save(function (err, user) {
+        passport.authenticate('local')(req, res, function () {
+          return res.status(200).json({
+            status: 'Registration Successful!'
+          });
+        });
+      });
     });
 });
 
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', function (err, user, info) {
     if (err) {
       return next(err);
     }
@@ -34,25 +49,25 @@ router.post('/login', function(req, res, next) {
         err: info
       });
     }
-    req.logIn(user, function(err) {
+    req.logIn(user, function (err) {
       if (err) {
         return res.status(500).json({
           err: 'Could not log in user'
         });
       }
-        
+
       var token = Verify.getToken(user);
-              res.status(200).json({
+      res.status(200).json({
         status: 'Login successful!',
         success: true,
         token: token
       });
     });
-  })(req,res,next);
+  })(req, res, next);
 });
 
-router.get('/logout', function(req, res) {
-    req.logout();
+router.get('/logout', function (req, res) {
+  req.logout();
   res.status(200).json({
     status: 'Bye!'
   });
